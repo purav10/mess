1491b994e700eba3013c7f5e3d08cdd56f46a947
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-05-14 10:03:04 +0530

    nn

--- messWebsite/settings.py
+++ messWebsite/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mess-production.up.railway.app','*']
+ALLOWED_HOSTS = ['mess-production.up.railway.app']
 
 
 # Application definition
